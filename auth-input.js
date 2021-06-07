10662c99e41cafa54ef6bca77b0a6072c77575c8
thomaswilburn <twilburn@npr.org>
2021-06-07 13:24:11 -0500

    Scaffolding

---
+++ auth-input.js
@@ -0,0 +1,16 @@
+import ElementBase from "./lib/elementBase.js";
+
+class AuthCheckpoint extends ElementBase {
+  constructor() {
+    super();
+
+  }
+
+  static template = `
+<style>
+
+</style>
+  `
+}
+
+AuthCheckpoint.define("auth-checkpoint");
