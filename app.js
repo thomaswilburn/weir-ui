3e8fcfd6ec0b4f21c4de52c2746efd4d957a5bda
Thomas Wilburn <news@thomaswilburn.net>
2021-06-14 08:35:11 -0500

    Add theme selector

---
+++ app.js
@@ -0,0 +1,23 @@
+import "./connection-status.js";
+import "./story-list.js";
+import "./story-renderer.js";
+import "./lib/keys.js";
+
+var themeBox = document.querySelector(".theme-select");
+var onThemeChange = function() {
+  var selected = themeBox.querySelector(":checked");
+  var theme = selected ? selected.value : "auto";
+  document.body.dataset.theme = theme;
+  localStorage.setItem("weirTheme", theme);
+}
+
+if (localStorage.getItem("weirTheme")) {
+  var theme = localStorage.getItem("weirTheme");
+  var input = themeBox.querySelector(`[value="${theme}"]`);
+  if (input) {
+    input.checked = true;
+  }
+}
+
+themeBox.addEventListener("change", onThemeChange);
+onThemeChange();
